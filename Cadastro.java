178c02def17caa810289fb53a65d87dea196d438
Malu Fernandes <85230561+malufernandes@users.noreply.github.com>
2021-08-01 18:40:33 -0300

    Update Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -19,34 +19,43 @@ public class Cadastro {
 		boolean desejaContinuar = false;
 
 		do {
-			System.out.println("Digite um nome: ");
-			String nome = scanner.nextLine();
-
-			System.out.println("Digite a idade: ");
-			Integer idade = scanner.nextInt();
-
-			var Pessoa = new pessoa(nome, idade);
+			
+			pessoa Pessoa = lerInformacoesDaPessoa(scanner);
+			
 			repositorio.salvar(Pessoa);
 
-			System.out.println();
-			scanner.nextLine();
-
-			System.out.println("Deseja continuar? (s) ou (n)");
-			String resposta = scanner.nextLine();
-
-			if (resposta.equals("s")) {
-
-				desejaContinuar = true;
-
-			} else {
-				desejaContinuar = false;
-			}
+			desejaContinuar = questionarContinuacao(scanner);
 
 		} while (desejaContinuar);
-		
+
 		for (var Pessoa : repositorio.buscarTodos()) {
 			System.out.println(Pessoa);
 		}
+
+	}
+	
+	public static pessoa lerInformacoesDaPessoa(Scanner scanner) {
+		
+		System.out.println("Digite um nome: ");
+		String nome = scanner.nextLine();
+
+		System.out.println("Digite a idade: ");
+		Integer idade = scanner.nextInt();
+		
+		limparScanner(scanner);
+		
+		return new pessoa(nome, idade);
+		
+	}
+
+	public static void limparScanner(Scanner scanner) {
+		scanner.nextLine();
+	}
+
+	public static boolean questionarContinuacao(Scanner scanner) {
+		System.out.println("Deseja continuar? (s) ou (n)");
+		var resposta = scanner.nextLine();
+		return resposta.equals("s");
 
 	}
 
